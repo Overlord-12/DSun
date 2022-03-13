9cd931c182e126659c43ade4afad35af1abd0bd1
Daniil Argastsev <daniil.argastsev@solbeg.com>
2022-03-13 18:55:39 +0300

    Delete unused fields

--- ServiceLibrary/DataConvertService.cs
+++ ServiceLibrary/DataConvertService.cs
@@ -31,37 +31,29 @@ namespace ServiceLibrary
                             var rowcount = worksheet.Dimension.Rows;
                             for (int row = 5; row <= rowcount; row++)
                             {
-
-                                var date = worksheet.Cells[row, 1].Value?.ToString()?.Trim();
-                                var time = worksheet.Cells[row, 2].Value?.ToString()?.Trim();
-                                var temperature = worksheet.Cells[row, 3].Value?.ToString()?.Trim();
-                                var wetness = worksheet.Cells[row, 4].Value?.ToString()?.Trim();
-                                var dewPoint = worksheet.Cells[row, 5].Value?.ToString()?.Trim();
-                                var pressure = worksheet.Cells[row, 6].Value?.ToString()?.Trim();
-                                var windDirection = worksheet.Cells[row, 7].Value?.ToString()?.Trim();
-                                var windSpeed = worksheet.Cells[row, 8].Value?.ToString()?.Trim();
-                                var cloudCover = worksheet.Cells[row, 9].Value?.ToString()?.Trim();
-                                var lowerCloudCover = worksheet.Cells[row, 10].Value?.ToString()?.Trim();
-                                var horizontalVisibility = worksheet.Cells[row, 11].Value?.ToString()?.Trim();
-                                var natureInvent = worksheet.Cells[row, 12].Value?.ToString()?.Trim();
-
-
                                 try
                                 {
                                     list.Add(new WeatherStatistic
                                     {
-                                        Date = Convert.ToDateTime(date),
-                                        Time = Convert.ToDateTime(time),
-                                        Temperature = !string.IsNullOrEmpty(temperature) ? Convert.ToDouble(temperature) : 0,
-                                        Wetness = !string.IsNullOrEmpty(wetness) ? Convert.ToDouble(wetness) : 0,
-                                        DewPoint = !string.IsNullOrEmpty(dewPoint) ? Convert.ToDouble(dewPoint) : 0,
-                                        Pressure = !string.IsNullOrEmpty(pressure) ? Convert.ToInt32(pressure) : 0,
-                                        WindDirection = windDirection,
-                                        WindSpeed = !string.IsNullOrEmpty(windSpeed) ? Convert.ToInt32(windSpeed) : 0,
-                                        CloudCover = !string.IsNullOrEmpty(cloudCover) ? Convert.ToInt32(cloudCover) : 0,
-                                        LowerCloudCover = !string.IsNullOrEmpty(lowerCloudCover) ? Convert.ToInt32(lowerCloudCover) : 0,
-                                        HorizontalVisibility = horizontalVisibility,
-                                        NatureInvent = natureInvent
+                                        Date = Convert.ToDateTime(worksheet.Cells[row, 1].Value?.ToString()?.Trim()),
+                                        Time = Convert.ToDateTime(worksheet.Cells[row, 2].Value?.ToString()?.Trim()),
+                                        Temperature = !string.IsNullOrEmpty(worksheet.Cells[row, 3].Value?.ToString()?.Trim())
+                                        ? Convert.ToDouble(worksheet.Cells[row, 3].Value?.ToString()?.Trim()) : 0,
+                                        Wetness = !string.IsNullOrEmpty(worksheet.Cells[row, 4].Value?.ToString()?.Trim()) 
+                                        ? Convert.ToDouble(worksheet.Cells[row, 4].Value?.ToString()?.Trim()) : 0,
+                                        DewPoint = !string.IsNullOrEmpty(worksheet.Cells[row, 5].Value?.ToString()?.Trim()) ? 
+                                        Convert.ToDouble(worksheet.Cells[row, 5].Value?.ToString()?.Trim()) : 0,
+                                        Pressure = !string.IsNullOrEmpty(worksheet.Cells[row, 6].Value?.ToString()?.Trim()) 
+                                        ? Convert.ToInt32(worksheet.Cells[row, 6].Value?.ToString()?.Trim()) : 0,
+                                        WindDirection = worksheet.Cells[row, 8].Value?.ToString()?.Trim(),
+                                        WindSpeed = !string.IsNullOrEmpty(worksheet.Cells[row, 8].Value?.ToString()?.Trim())
+                                        ? Convert.ToInt32(worksheet.Cells[row, 8].Value?.ToString()?.Trim()) : 0,
+                                        CloudCover = !string.IsNullOrEmpty(worksheet.Cells[row, 9].Value?.ToString()?.Trim()) 
+                                        ? Convert.ToInt32(worksheet.Cells[row, 9].Value?.ToString()?.Trim()) : 0,
+                                        LowerCloudCover = !string.IsNullOrEmpty(worksheet.Cells[row, 10].Value?.ToString()?.Trim()) 
+                                        ? Convert.ToInt32(worksheet.Cells[row, 10].Value?.ToString()?.Trim()) : 0,
+                                        HorizontalVisibility = worksheet.Cells[row, 11].Value?.ToString()?.Trim(),
+                                        NatureInvent = worksheet.Cells[row, 12].Value?.ToString()?.Trim()
                                     });
                                 }
                                 catch (Exception ex)
